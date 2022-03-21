1a0681e71bfd8fca078e62f23d770c3022fefbc5
Rosa Regalado <rosaregalado101@gmail.com>
2022-03-20 23:23:59 -0700

    adding middleware

--- app.js
+++ app.js
@@ -1,14 +1,25 @@
+// server
 const express = require('express');
 const app = express();
 
-//middleware
+// Middleware
+const exphbs  = require('express-handlebars');
+app.engine('handlebars', exphbs.engine({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
 
-// routes
+// Routes
 app.get('/', (req, res) => {
-  res.send('Hello Squirrel');
+  const gifUrl = 'https://media1.tenor.com/images/561c988433b8d71d378c9ccb4b719b6c/tenor.gif?itemid=10058245'
+  // render the hello-gif view, passing the gifUrl into the view to be displayed
+  res.render('hello-gif', { gifUrl });
 });
 
-//start server
+app.get('/greetings/:name', (req, res) => {
+  // grab name from the path
+  const name = req.params.name;
+  res.render('greetings', { name });
+})
+// Start server
 app.listen(3000, () => {
   console.log('Gif Search listening on port localhost:3000!');
 });
